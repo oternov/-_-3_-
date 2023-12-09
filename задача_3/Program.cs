1ff5937bebcf1a6173af7a49dd27d87eb1f964fa
Olga <tov_84@inbox.ru>
2023-12-09 23:30:04 +0300

    Округление значений в задаче 3 до 2 знаков после запятой

--- задача_3/Program.cs
+++ задача_3/Program.cs
@@ -1,12 +1,14 @@
 ﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 
+using System.ComponentModel;
+
 double[] array = new double[7];
 
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().NextDouble();
-    Console.Write(array[i] + " ");
+    Console.Write(array[i].ToString("F2") + " "); // вывод массива с округлением до двух знаков после запятой
 }
 
 double max = array[0];
@@ -25,10 +27,13 @@ for (int i = 0; i < array.Length; i++)
 }
 
 Console.WriteLine();
-Console.WriteLine("максимум " + max);
-Console.WriteLine("минимум " + min);
+
+string smax = max.ToString("F2"); // преобразование числа max до двух знаков после запятой
+string smin = min.ToString("F2"); // преобразование числа min двух знаков после запятой
+Console.WriteLine("максимум " + smax);
+Console.WriteLine("минимум " + smin);
 
 double result = max - min;
-string res = string.Format("{0:f}", result); // преобразование вывода до двух знаков после запятой
+string res = result.ToString("F2"); // преобразование вывода до двух знаков после запятой
 
 Console.WriteLine ("Разница между максимальным и минимальным элементом равна " + res);
