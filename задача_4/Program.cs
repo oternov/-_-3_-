f0e7bd56fb0a53f8011d33d9af26d4e7074337ce
Olga <tov_84@inbox.ru>
2023-12-09 17:09:38 +0300

    Решена задача 4

--- задача_4/Program.cs
+++ задача_4/Program.cs
@@ -14,18 +14,23 @@ while (P % 10 > 0)
 }
 
 int[] array = new int[K];
+int[] array2 = new int[K];
 
 if (N >= 1 && N <= 100000)
 {
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = N % 10;
-        N = N/10;
-        Console.Write(array[i] + " ");
+        N = N/10;  
     }
+    for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = array[array.Length-1-i];
+        Console.Write(array2[i] + " ");
+    }
+
 }
 else
 {
     Console.WriteLine("Число не подходит");
 }
-
