620ec6323d1062a151bd339766185496c17e14fe
Olga <tov_84@inbox.ru>
2023-12-09 22:17:04 +0300

    Откорректирован поиск минимального элемента

--- задача_3/Program.cs
+++ задача_3/Program.cs
@@ -2,15 +2,18 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 
 double[] array = new double[7];
-double max = array[0];
-double min = array[0];
-
 
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().NextDouble();
     Console.Write(array[i] + " ");
+}
 
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
     if (array[i] > max)
     {
         max = array[i];
@@ -25,7 +28,7 @@ Console.WriteLine();
 Console.WriteLine("максимум " + max);
 Console.WriteLine("минимум " + min);
 
-
 double result = max - min;
+string res = string.Format("{0:f}", result); // преобразование вывода до двух знаков после запятой
 
-Console.WriteLine ("Разница между максимальным и минимальным элементом равна " + result);
+Console.WriteLine ("Разница между максимальным и минимальным элементом равна " + res);
